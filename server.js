f952df3ae99031f14cb01a8b654ec7f2aca04d7d
359Peeratat <peerrogame123@gmail.com>
2025-08-03 00:07:15 +0700

    02

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const multer = require('multer');
 const lineWebhook = require('./webhook/line-webhook');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
+// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
